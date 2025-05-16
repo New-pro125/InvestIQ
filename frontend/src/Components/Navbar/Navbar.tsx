7e447d4794c5c2ff84406b0783de394942e2067a
unknown <ahmed125.am13@gmail.com>
2025-05-16 23:28:48 +0300

    Finish the project

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "./logo.png";
-import "./Navbar.css";
-import { useAuth } from "../../Context/useAuth";
+import React from "react"
+import {Link} from "react-router-dom"
+import logo from "./logo.png"
+import "./Navbar.css"
+import {useAuth} from "../../Context/useAuth"
 
 interface Props {}
 
 const Navbar = (props: Props) => {
-  const { isLoggedIn, user, logout } = useAuth();
+  const {isLoggedIn, user, logout} = useAuth()
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-20">
-          <Link to="/">
-            <img src={logo} alt="" />
+          <Link to="/" className="underline text-2xl">
+            InvestIQ
           </Link>
           <div className="hidden font-bold lg:flex">
             <Link to="/search" className="text-black hover:text-darkBlue">
@@ -46,7 +46,7 @@ const Navbar = (props: Props) => {
         )}
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
